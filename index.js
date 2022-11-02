df63ee4a58d69cdd3117c150bcc9ecb7f8b81533
Anna Harder <annaqharder@gmail.com>
2022-11-02 16:10:34 -0600

    commit tree 2922098a75244f5fe16712be3e000a2d9dd1f9ee

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+// function findMatching(drivers, name) {
+//     for(const person of drivers) {
+//         if(drivers === name) {
+//             return drivers
+//         }
+//     }
+// }
+
+// filter(drivers, "Bobby") 
+
+function findMatching(drivers, name) {
+    return drivers.filter((possibleMatch) => possibleMatch.toLowerCase() === name.toLowerCase()
+    ); 
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter((possibleMatch) => possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0
+    ); 
+}
+
+function matchName(drivers, newString) {
+    return drivers.filter((possibleMatch) => possibleMatch.name === newString); 
+}
